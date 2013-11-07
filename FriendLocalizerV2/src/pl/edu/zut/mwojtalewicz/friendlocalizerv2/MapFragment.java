c99a54fc3f23cf418b3521ef9d34619b2260903e
Michal <michal-sajmon@wp.pl>
2013-11-07 13:11:47 +0100

    Działająca Mapa
    Naprawiono mapę

--- FriendLocalizerV2/src/pl/edu/zut/mwojtalewicz/friendlocalizerv2/MapFragment.java
+++ FriendLocalizerV2/src/pl/edu/zut/mwojtalewicz/friendlocalizerv2/MapFragment.java
@@ -1,5 +1,12 @@
 package pl.edu.zut.mwojtalewicz.friendlocalizerv2;
 
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+import pl.edu.zut.mwojtalewicz.Library.ConnectionDetector;
+import pl.edu.zut.mwojtalewicz.Library.DataBaseHandler;
+import pl.edu.zut.mwojtalewicz.Library.UserFunctions;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -8,6 +15,9 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends android.support.v4.app.Fragment {
 	private GoogleMap googleMap;
@@ -18,12 +28,13 @@ public class MapFragment extends android.support.v4.app.Fragment {
 		
 	    View v = inflater.inflate(R.layout.map_fragment, container, false);
 
-	    setUpMapIfNeeded(v);
+	    setUpMapIfNeeded();
 	    return v;
 		
 	}
  
-	private void setUpMapIfNeeded(View v) {
+	private void setUpMapIfNeeded() 
+	{
 		
 		if (googleMap == null) {
 	    	mMapFragment = ((SupportMapFragment)getFragmentManager().findFragmentById(R.id.map));
@@ -36,11 +47,44 @@ public class MapFragment extends android.support.v4.app.Fragment {
 	        	googleMap.getUiSettings().setMyLocationButtonEnabled(true);
 	        }
 	    }
-	    
+		
+		ConnectionDetector cd = new ConnectionDetector(getActivity()); 
+		Boolean isInternetPresent = cd.isConnectingToInternet();
+		
+		if(isInternetPresent == true)
+		{
+			DataBaseHandler db = new DataBaseHandler(getActivity());
+			HashMap<String, String> userDetails = db.getUserDetails();
+			UserFunctions usr = new UserFunctions();
+			String uniqueID = userDetails.get("uid");
+			JSONObject json = usr.showUserFriendsLocatons(uniqueID);
+			
+			String name, lastname, longitude, latitude;
+			try {
+				int userNum = Integer.parseInt(json.getString("usersNumber"));
+				JSONObject jsonUser;
+				for(int i = 0; i < userNum; i++)
+				{
+					jsonUser = json.getJSONObject(""+i);
+					uniqueID = jsonUser.getString("unique_id");
+					name = jsonUser.getString("name");
+					lastname = jsonUser.getString("lastname");
+					longitude = jsonUser.getString("longitude");
+					latitude = jsonUser.getString("latitude");
+					
+					MarkerOptions marker = new MarkerOptions().position(new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude))).title(name + " " +lastname);
+					marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+					
+					googleMap.addMarker(marker).showInfoWindow();
+				}
+			} catch (Exception e) {e.printStackTrace();}
+		}	    
 	}
 	
+	@Override
 	public void onDestroyView ()
 	{
+	    super.onDestroyView();  
 	      try{
 	    	  SupportMapFragment fragment = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map));
 	    	  FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
@@ -48,7 +92,12 @@ public class MapFragment extends android.support.v4.app.Fragment {
 	    	  ft.commit();
 	      }catch(Exception e){
 	      }
-	    super.onDestroyView();  
 	}
-	
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		setUpMapIfNeeded();
+	}
 }
